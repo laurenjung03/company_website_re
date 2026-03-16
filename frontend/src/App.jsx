f44ccaa1eaaa00e500efa415c82899006677e563
user <user@userui-MacBookPro.local>
2026-03-17 00:30:56 +0900

    basic footer까 지:router마저설정하기

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,35 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
-
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+const Layout = () => {
+  <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </>;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [{ index: true, element: <MainPage /> }, {}],
+  },
+]);
 
 function App() {
   return (
     <>
       <div>
-        <Navbar />
-        <Footer />
+        {/* <RouterProvider router={router}/> */}
+        <BrowserRouter>
+          <Navbar />
+          <Footer />
+        </BrowserRouter>
       </div>
     </>
   );
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,13 +1,81 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaInstagram } from "react-icons/fa";
+import { CiTwitter } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+import { RiKakaoTalkLine } from "react-icons/ri";
 
 function Footer() {
+  const scrolltoTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
     <div>
-      <footer className="w-full bg-gray-900 text-gray-300 ">
+      <footer className="bg-gray-900 text-gray-300 ">
         <div className="container mx-auto px-4 py-12">
-          <div className="">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
             <div>
-              <h3 className="text-2xl font-bold">회사소개</h3>
+              <h3 className="text-xl mb-4 font-bold">회사소개</h3>
+              <p className="text-gray-300">
+                저희 회사는 최고의 서비스를 제공합니다
+              </p>
+            </div>
+            <div>
+              <h3 className="text-xl mb-4 font-bold">빠른 링크</h3>
+              <ul className="space-y-4">
+                <li>
+                  {" "}
+                  <Link to="/" onClick={scrolltoTop}>
+                    홈{" "}
+                  </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/about" onClick={scrolltoTop}>
+                    회사정보{" "}
+                  </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/leadership" onClick={scrolltoTop}>
+                    임원소개{" "}
+                  </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/board">업무게시판 </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/our-service" onClick={scrolltoTop}>
+                    제공기술{" "}
+                  </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/contact" onClick={scrolltoTop}>
+                    문의{" "}
+                  </Link>{" "}
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h3 className="text-xl mb-4 font-bold">연락처</h3>
+              <ul className="space-y-2 text-gray-400">
+                <li>서울특별시 강남구</li>
+                <li>삼성동123</li>
+                <li>전화: 02-933</li>
+                <li>이메일: inf@example.com</li>
+              </ul>
+            </div>
+            <div>
+              <h3 className="text-xl mb-4 font-bold">소셜미디어</h3>
+              <div className="flex gap-4">
+                <CiLinkedin />
+                <RiKakaoTalkLine />
+                <CiTwitter />
+                <FaInstagram />
+              </div>
             </div>
           </div>
         </div>
